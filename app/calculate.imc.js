37858679c2cd101661817a805ab81bb4948b016d
emilio.resende <emilio.murta@msitbrasil.com>
2023-02-26 21:32:19 -0300

    add calculate through API

--- app/calculate.imc.js
+++ app/calculate.imc.js
@@ -1,29 +1,13 @@
 import { debounce } from './debounce.js';
 
-function withDescription() {
-    if (this.imc < 18.5) this.imcDescription = "magreza";
-    else if (this.imc <= 24.9) this.imcDescription = "normal";
-    else if (this.imc <= 29.9) this.imcDescription = "sobrepeso";
-    else this.imcDescription = "obesidade";
+import { calculateImcAPI } from './drivers/calculate.imc.api.js';
+import { Person } from './models/domain.js';
 
-    return this;
-}
-
-function doCalculateImc(height, weight) {
-    const obj = {};
-    obj.height = height;
-    obj.weight = weight;
-    obj.imc = parseFloat((weight / (height ** 2)).toFixed(2));
-    obj.withDescription = withDescription.bind(obj);
-
-    return obj;
-}
-
-export function calculateImc(height, weight) {
+export function calculateImc(height, weight, callback) {
     if (height == null || height == 0) return null;
     if (weight == null || weight == 0) return null;
 
-    return doCalculateImc(height, weight).withDescription();
+    return calculateImcAPI(new Person(height, weight), callback);
 }
 
 export function calculate() {
@@ -35,11 +19,11 @@ export function calculate() {
     if (heightEl) height = heightEl.value;
     if (weightEl) weight = weightEl.value;
 
-    let response = calculateImc(height, weight);
-
-    if (response && response.imc) {
-        document.querySelector("#imc").innerHTML = `${response.imc}, ${response.imcDescription}`;
-    }
+    calculateImc(height, weight, response => {
+        if (response && response.imc) {
+            document.querySelector("#imc").innerHTML = `${response.imc}, ${response.imcDescription}`;
+        }
+    });
 }
 
 export function initialize() {
